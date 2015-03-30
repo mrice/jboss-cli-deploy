b50cc2446114e7747814b871341543fab96fe0d3
Michael Rice <mrice@redhat.com>
2015-03-30 16:28:55 -0700

    parameterized the values for jenkins

--- src/main/assembly/deployment.sh
+++ src/main/assembly/deployment.sh
@@ -2,10 +2,10 @@
 # general steps
 
 # get a copy of the artifact from internal artifactory/nexus
-mvn dependency:get -Dartifact=com.michaelrice.demo:hello-world:1.0-SNAPSHOT:war
+mvn dependency:get -Dartifact=${groupId}:${artifact}:${version}:${packaging}
 
 # copy the artifact from .m2/repository to workspace
-mvn dependency:copy -Dartifact=com.michaelrice.demo:hello-world:1.0-SNAPSHOT:war
+mvn dependency:copy -Dartifact=${groupId}:${artifact}:${version}:${packaging}
 
 # now deploy to jboss as/eap
-jboss-cli.sh --connect --command="deploy ./target/dependency/hello-world-1.0-SNAPSHOT.war"
+jboss-cli --connect --controller=${controller} --user=${as_user} --password=${as_password} --command="deploy ./target/dependency/${artifact}-${version}.${packaging} --server-groups=${server_group}"
